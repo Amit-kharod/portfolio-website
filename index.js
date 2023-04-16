6ebdf7e24710ac73d6585865902fef5d3771a889
Amit-kharod <kharodcloud1@gmail.com>
2023-04-16 17:58:20 +0530

    added meta tags for seo

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const sections = document.querySelectorAll('section');
-const links = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll("nav a");
 
-window.addEventListener('scroll', ()=>{
-    let current = '';
+window.addEventListener("scroll", () => {
+  let current = "";
 
-    sections.forEach((section) => {
+  sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
 
     if (pageYOffset >= sectionTop - sectionHeight / 5) {
       current = section.getAttribute("id");
     }
-    })
-    console.log(current)
-    links.forEach((link) => {
-        link.classList.remove("active");
-        if (link.classList.contains(current)) {
-            link.classList.add("active");
-        }
-      });
-})
-
+  });
+  console.log(current);
+  links.forEach((link) => {
+    link.classList.remove("active");
+    if (link.classList.contains(current)) {
+      link.classList.add("active");
+    }
+  });
+});
